8b34b0b0cc4d1016f14961ab3d83cc601c0685fb
peykedarya <peykedarya@gmail.com>
2016-10-29 02:26:53 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,8 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
-    disabled_channels = {}
+    sudo_users = {Trade_education}    disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
   print ('saved config into ./data/config.lua')
